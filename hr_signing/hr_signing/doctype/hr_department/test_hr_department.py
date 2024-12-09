cc7d31fcdece09f98079817cc79017b297ba1fef
bharat <bharat-atn>
2024-12-09 17:11:34 +0530

    my first commit

---
+++ hr_signing/hr_signing/doctype/hr_department/test_hr_department.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Bharat and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestHRDepartment(FrappeTestCase):
+	pass
